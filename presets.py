eda5a73fc0e6f10604aeeef780251a9510eba77a
alex <alexbilla02@gmail.com>
2026-04-08 12:09:26 +0200

    refactoring documentation path

--- presets.py
+++ presets.py
@@ -1,23 +1,17 @@
 SHIP_PRESETS = {
+    "Veloce (2 navi)": [
+        ("Sottomarino",        3),
+        ("Cacciatorpediniere", 2),
+    ],
+    "Istantanea (1 nave)": [
+        ("Cacciatorpediniere", 2),
+    ],
     "Classica (5 navi)": [
         ("Portaerei",          5),
         ("Corazzata",          4),
         ("Incrociatore",       3),
         ("Sottomarino",        3),
         ("Cacciatorpediniere", 2),
-    ],
-    "Veloce (1 nave)": [
-       
-        ("Cacciatorpediniere", 2),
-    ],
-    "Intensa (7 navi)": [
-        ("Portaerei",          5),
-        ("Corazzata",          4),
-        ("Incrociatore",       3),
-        ("Sottomarino",        3),
-        ("Cacciatorpediniere", 2),
-        ("Pattugliatore",      2),
-        ("Lancia",             1),
     ],
 }
 
